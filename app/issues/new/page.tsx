94dd1d2f092014de4e5441e30d05727b27340a73
Jon <jonprosser84@gmail.com>
2024-10-20 13:04:34 +0100

    extract error logic

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,34 @@
 "use client";
 
-import { Button, Callout, CalloutRoot, Text, TextField } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  CalloutRoot,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from "@hookform/resolvers/zod";
 import { issueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
-type IssueForm = z.infer<typeof issueSchema>
+type IssueForm = z.infer<typeof issueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
-    resolver: zodResolver(issueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(issueSchema),
   });
   const [error, setError] = useState("");
 
@@ -41,14 +53,14 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
             <>
               <SimpleMDE placeholder="Description" {...field} />
-              {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+              <ErrorMessage>{errors.description?.message}</ErrorMessage>
             </>
           )}
         />
@@ -56,6 +68,6 @@ const NewIssuePage = () => {
       </form>
     </div>
   );
-}
+};
 
 export default NewIssuePage;
